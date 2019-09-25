e7a71e2d45db7bb6c618809e9bc1572211b2f3bd
gloomy <gloomyneter@gmail.com>
2019-09-25 18:42:31 +0800

    build

--- cpp/build_linux.sh
+++ cpp/build_linux.sh
@@ -1,2 +1,3 @@
 rm -rf *.o *.so *.c.*.*
-gcc -I/root/JDK1_8/include/ -I/root/JDK1_8/include/linux -shared -lbz2 -o libGdiff.so AndroidApkUtils.c
+gcc -I/root/JDK1_8/include/ -I/root/JDK1_8/include/linux -shared -lbz2 -fPIC -c AndroidApkUtils.c
+gcc -shared  AndroidApkUtils.o -o libGdiff.so
