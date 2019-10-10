b1e00c8a83d7816aa7480d67d1a788750d0de7dd
gloomy <gloomyneter@gmail.com>
2019-10-10 13:00:07 +0800

    mac so

--- cpp/build_mac.sh
+++ cpp/build_mac.sh
@@ -1,4 +1,4 @@
 rm -rf *.o *.so
 gcc -dynamiclib -I/Library/Java/JavaVirtualMachines/jdk1.8.0_221.jdk/Contents/Home/include -I/Library/Java/JavaVirtualMachines/jdk1.8.0_221.jdk/Contents/Home/include/darwin -lbz2 -o libGdiff.so AndroidApkUtils.c
 rm -rf ../src/main/resources/jniLibs/mac/*
-# mv libGdiff.so ../src/main/resources/jniLibs/mac/
+mv libGdiff.so ../src/main/resources/jniLibs/mac/
